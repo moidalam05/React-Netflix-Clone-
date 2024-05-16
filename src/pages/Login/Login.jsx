5f935ffd198d6c5739a73c174daa2604c7ff92ae
moidalam05 <moidalam005@hotmail.com>
2024-05-17 00:55:20 +0530

    Add loading spinner during login process

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -2,22 +2,30 @@ import React, { useState } from 'react';
 import './Login.css';
 import logo from '../../assets/logo.png';
 import { login, signup } from '../../firebase';
+import netflix_spinner from '../../assets/netflix_spinner.gif';
 
 const Login = () => {
 	const [signState, setSignState] = useState('Sign In');
 	const [name, setName] = useState('');
 	const [email, setEmail] = useState('');
 	const [password, setPassword] = useState('');
+	const [loading, setLoading] = useState(false);
 
 	const user_auth = async (event) => {
 		event.preventDefault();
+		setLoading(true);
 		if (signState === 'Sign In') {
 			await login(email, password);
 		} else {
 			await signup(name, email, password);
 		}
+		setLoading(false);
 	};
-	return (
+	return loading ? (
+		<div className='login-spinner'>
+			<img src={netflix_spinner} alt='netflix spinner' />
+		</div>
+	) : (
 		<div className='login'>
 			<img src={logo} alt='logo' className='login-logo' />
 			<div className='login-form'>
