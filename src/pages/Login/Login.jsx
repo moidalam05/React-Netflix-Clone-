17923288d9307a9e9d997c7e7930bdad95ff9560
moidalam05 <moidalam005@hotmail.com>
2024-05-16 11:53:18 +0530

    Add react-router-dom dependency and implement routing in App.jsx and main.jsx

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,8 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css';
+import logo from '../../assets/logo.png';
 
 const Login = () => {
-	return <div className='login'>Login</div>;
+	const [signState, setSignState] = useState('Sign In');
+	return (
+		<div className='login'>
+			<img src={logo} alt='logo' className='login-logo' />
+			<div className='login-form'>
+				<h1>{signState}</h1>
+				<form>
+					{signState === 'Sign Up' ? (
+						<input type='text' placeholder='Your name' />
+					) : (
+						<></>
+					)}
+					<input type='email' placeholder='Email' />
+					<input type='password' placeholder='Password' />
+					<button>{signState}</button>
+					<div className='form-help'>
+						<div className='remember'>
+							<input type='checkbox' />
+							<label htmlFor=''>Remember me</label>
+						</div>
+						<p>Need Help?</p>
+					</div>
+				</form>
+				<div className='form-switch'>
+					{signState === 'Sign In' ? (
+						<p>
+							New to Netflix?
+							<span
+								onClick={() => {
+									setSignState('Sign Up');
+								}}
+							>
+								Sign Up Now
+							</span>
+						</p>
+					) : (
+						<p>
+							Already have account?
+							<span
+								onClick={() => {
+									setSignState('Sign In');
+								}}
+							>
+								Sign In Now
+							</span>
+						</p>
+					)}
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Login;
